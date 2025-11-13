62c7fdbd20dd3ad6897774c8b26116138bfd305d
gabegekas <gabe@gekas.org>
2025-11-13 08:14:07 -0800

    update sound speed

--- TDOA2angle.m
+++ TDOA2angle.m
@@ -4,13 +4,17 @@ clc;
 
 author = "Gabriel Gekas";
 
+filename = 'DTOA_AllLocations_OnlyFirstSampling.csv';
+tdoa = readtable(['Data/', filename]);
+
 heading = 180; % degrees heading in geodetic frame (north-0, +clockwise)
 tilt = 30; % degrees upward tilt of hydrophone array
 
 % water properties taken from https://data.caloos.org/
 T = 18.39; % Temperature [Celsius]
 S = 30.2; % Salinity [PSS]
-c = 1500; % m/s
+depth = 4; % m
+c = salt_water_c(T, depth, S);
 
 array_axis_size = 2; % m
 
@@ -217,4 +221,31 @@ ax = atan2d(p2(2) - p1(2), p2(1) - p1(1)); % axis in xy plane that the hydrophon
 ang = 90 - atan2d(Y, X); % heading of detection
 % ang = mod(ang+180, 360); 
 
+end
+
+function sspeed = salt_water_c(temp_C, depth, sal)
+%   Calculate sound speed [m/s] in sea water as function of temperature
+%   [degree C], depth [meter], and salinity in parts per thousand [ppt] 
+%   using nine-term Mackenzie equation that yelds standard error of sound 
+%   speed estimate =0.070m/s.  For near surface depth, typical values of salinity 
+%   are in the range from 30 to 40 ppt (at t=0-30C).  Examples of calculations: 
+%   1511.2= salt_water_c(17, 1, 33.6); 
+%   1482.3 = salt_water_c(3.51, 1200, 33.51);
+%
+% References:
+% Mackenzie, K.V., "Nine-term Equation for Sound Speed in the Oceans", 
+% JASA, vol. 70, No. 3, Sept. 1981, pp. 807 - 812
+% Mackenzie, K.V., "Discussion of Sea Water Sound-speed Determinations",
+% JASA, vol. 70, No. 3, Sept. 1981  pp. 801 - 806
+
+%_____________________________________________________
+% 	Sergei Koptenko, Resonant Medical Inc., Toronto  |
+%	sergei.koptenko@resonantmedical.com              |
+%______________April/30/2004_________________________|
+
+
+sspeed  = 1448.96 + 4.591 .* temp_C - 5.304 *10^-2 .* temp_C .^2 + 2.374*10^-4 .* temp_C .^3 ...
+    +1.340 .* (sal-35) + 1.630 *10^-2 .* depth + 1.675 *10^-7 .* depth .^2 ...
+    -1.025*10^-2 .* temp_C .* (sal-35) - 7.139 *10^-13 .* temp_C .*depth .^3; 
+    
 end
